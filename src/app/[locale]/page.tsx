dddd6d8e4801dc59d82556b11c6c38d1d7bdf1d3
Zakaria Sichaib <zakisb97@gmail.com>
2024-03-13 17:41:47 +0100

    Add button component to page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 export default function Page() {
   return (
-    <Link href="/auth/login">Click here to be redirected to the auth page</Link>
+    <div className="h-screen flex items-center justify-center">
+      <Link href="/auth/login">
+        <Button> Click here to be redirected to the auth page </Button>
+      </Link>
+    </div>
   );
 }
